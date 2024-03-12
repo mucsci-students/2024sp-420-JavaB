32d45f1d43b9a5e0da44100329818ac323044a9d
ramgurung7743 <156702112+ramgurung7743@users.noreply.github.com>
2024-03-12 01:28:26 -0400

    Updated Project

---
+++ UMLEditorSprints/lib/src/main/java/Model/Relationship.java
@@ -0,0 +1,132 @@
+package Model;
+
+/**
+ * Represents a relationship between two classes in a UML diagram.
+ */
+public class Relationship {
+
+    private String source;
+    private String destination;
+    private int relationType;
+
+   
+    /**
+     * Constructs a Relationship object with specified attributes.
+     *
+     * @param source      The name of the source class in the relationship.
+     * @param destination The name of the destination class in the relationship.
+     */
+   
+    /**
+     * Constructs a Relationship object with specified attributes.
+     *
+     * @param source      The name of the source class in the relationship.
+     * @param destination The name of the destination class in the relationship.
+     */
+    public Relationship(String source, String destination, int type) { 
+        this.source = source;
+        this.destination = destination;
+        this.relationType = type;       
+    }
+    
+    // A method to get a string representation of the relationship type
+    public static String getTypeAsString(int relationType) {
+        switch (relationType) {
+        	case 1:
+        		return "Association";
+            case 2:
+                return "Aggregation";
+            case 3:
+                return "Composition";
+            case 4:
+                return "Inheritance";
+            case 5:
+                return "Realization";
+            default:
+                return "Unknown";
+        }
+    }
+
+    /**
+     * Sets the source class in the relationship.
+     *
+     * @param source The name of the source class to set.
+     */
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    /**
+     * Sets the destination class in the relationship.
+     *
+     * @param destination The name of the destination class to set.
+     */
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    /**
+     * Gets the name of the source class in the relationship.
+     *
+     * @return The name of the source class.
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * Gets the name of the destination class in the relationship.
+     *
+     * @return The name of the destination class.
+     */
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getType(){
+        return relationType;
+    }
+    
+    public void setType(int type) {
+    	this.relationType = type;
+    }
+    
+    //add comments
+    public void changeRelType(int type) {
+        // Directly switch on the int type without converting to a String
+        switch (type) {
+            case 1:
+                // Assign the integer representing the relationship type
+                this.relationType = 1; // Association
+                break;
+            case 2:
+                // Assign the integer representing the relationship type
+                this.relationType = 2; // Aggregation
+                break;
+            case 3:
+                this.relationType = 3; // Composition
+                break;
+            case 4:
+                this.relationType = 4; // Inheritance
+                break;
+            case 5:
+                this.relationType = 5; // Realization
+                break;
+            default:
+                // Optionally handle invalid types
+                System.out.println("Invalid relationship type");
+                break;
+        }
+    }
+
+    /**
+     * Returns a string representation of the Relationship object.
+     *
+     * @return A string representation including the source and destination classes.
+     */ 
+    @Override
+    public String toString() {
+        // Including type description in the toString output
+        return "Relationship is (" + getTypeAsString(relationType) + ")" + "\nbetween " + source + " and " + destination;
+    }
+}
