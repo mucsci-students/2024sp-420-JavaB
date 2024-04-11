88b46fac0830639ee54d94d4c5ff2e624c601374
BasilrockMU <70165455+BasilrockMU@users.noreply.github.com>
2024-04-11 16:20:17 -0400

    The drawing of the jpanel (component content panel is good now), however this does not solve the fact that some of the elements are hidden behind the window, so the snapshotting isn't achieved yet.

--- java2/src/main/java/com/classuml/Controller/UMLGui.java
+++ java2/src/main/java/com/classuml/Controller/UMLGui.java
@@ -2,6 +2,7 @@ package com.classuml.Controller;
 
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.IllegalComponentStateException;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
@@ -243,7 +244,7 @@ public class UMLGui extends JFrame implements ActionListener {
 		{
 			frameLocation = new Point();
 		}
-		int frameX = frameLocation.x + 9;
+		int frameX = frameLocation.x;
 		int frameY = frameLocation.y;
 		windowDimensions = classPanelContainer.getBounds();
 		windowDimensions.setLocation(frameX, frameY);
@@ -1334,13 +1335,26 @@ public class UMLGui extends JFrame implements ActionListener {
 		updateDiagramView();
 	}
 
-	private void getSnapshotImage() throws IOException {
+	private void getSnapshotImage() throws IOException 
+	{
 		// https://alvinalexander.com/blog/post/jfc-swing/how-take-create-screenshot-java-swing-robot-class/
+		// Make sure our window dimensions are caught up for snapshot.
 		updateDiagramView();
-		BufferedImage background = rbt.createScreenCapture(windowDimensions);
-		File outputfile = new File("image.jpg");
-		ImageIO.write(background, "jpg", outputfile);
+		BufferedImage bImg = new BufferedImage(getContentPane().getWidth(), getContentPane().getHeight(), BufferedImage.TYPE_INT_RGB);
+		Graphics2D cg = bImg.createGraphics();
+		getContentPane().paintAll(cg);
+		try
+		{
+			if (ImageIO.write(bImg, "png", new File("./output_image.png")))
+			{
+				System.out.println("-- saved");
+			}
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+    	}
 	}
+
 
 	
 /**************************************************************************************************************************************/
